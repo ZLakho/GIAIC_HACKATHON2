c36362912a2fc64d526cf59a4018fa037e38c33a
ZLakho <zainablakho2015@gmail.com>
2024-12-24 16:13:46 +0500

    second commit

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import BrowseRange from "@/components/browserange";
+import FuniroFurniture from "@/components/funirofurniture";
+import Hero from "@/components/hero";
+import Inspirations from "@/components/inspirations";
+import OurProducts from "@/components/ourproducts";
+
+export default function Home() {
+  return (
+    <>
+    <Hero/>
+    <BrowseRange/>  
+    <OurProducts/>
+    <Inspirations/>
+    <FuniroFurniture/>
+
+    </>
+  );
+}
